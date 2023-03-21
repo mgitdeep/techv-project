aae5fdfa601e952fefee6af401bdb1ab62f10a91
Mondeep Kalita <epicdeep09@gmail.com>
2023-03-21 17:49:16 +0530

    Deleted huge comments!

--- src/components/forms/MultipleInputs.js
+++ src/components/forms/MultipleInputs.js
@@ -1,22 +1,6 @@
 import React, { useState } from 'react'
 
 const MultipleInputs = () => {
-    // const [fname, setFname] = useState("");    // Here "name" and "pswd" are holding the User input values
-    // const [emaill, setEmaill] = useState("");
-    // const [phonee, setPhonee] = useState("");
-    // const [pswd, setPswd] = useState("");
-
-
-    // const [allEntry, setAllEntry] = useState([]);
-
-    // const submitForm = (e) => {
-    //     e.preventDefault();
-
-    //     const newEntry = { name: fname, email: emaill, phone: phonee, password: pswd, id: new Date().getTime().toString()}
-
-    //     setAllEntry([ ...allEntry, newEntry]);
-    //     console.log(allEntry);
-    // }
 
     const [userSignUpData, setUserSignUpData] = useState({
         firstname: "",
@@ -27,9 +11,7 @@ const MultipleInputs = () => {
 
     const [allRecords, setAllRecords] = useState([])
 
-    // const handleInput = (e) => {
-    //     setUserSignUp(e.target.value)            Not an efficient way. You can type in the fields but as soon as you type some thing console will show you can error
-    // }
+    
     const handleInput = (e) => {
         const nameee = e.target.name
         const valuee = e.target.value
@@ -43,7 +25,6 @@ const MultipleInputs = () => {
     const submitForm = (e) => {
         e.preventDefault()
 
-        // const allUserData = { ...userSignUpData, [nameee]: valuee }              - will give error: 'namee', 'valuee' is not defined - you know why it is :)
         const allUserData = { ...userSignUpData, id: new Date().getTime().toString()}
 
         setAllRecords([ ...allRecords, allUserData])
@@ -59,7 +40,6 @@ const MultipleInputs = () => {
                 <label htmlFor="firstname">First Name</label>
                 <input type="text" autoComplete='off' name='firstname' id='firstname' required
                 value={userSignUpData.firstname}
-                // onChange={e => handleInput(e)}/>
                 onChange={handleInput}/>
             </div>
             <div>
@@ -83,26 +63,12 @@ const MultipleInputs = () => {
             <button type='submit'>Sign up</button>
         </form>
         
-        {/* <div>
-            {
-                allEntry.map((curElm) => {
-                    const [name, email, id] = curElm                            // Array Destructuring
-                    return (
-                        <div className='showDataStyle' key={id}>        
-                           
-                            <p>{name}</p>
-                            <p>{email}</p>
-                        </div>
-                    )
-                })
-            }
-        </div> */}
+       
         <div>
             {
                 allRecords.map((curElm) => {
                     // const [firstname, email] = curElm                Here Array Destructuring won't work, use OBJECT DESTRUCTURING
                     const {firstname, email, id} = curElm
-                    // console.log(curElm)
                     return (
                         <div className='showDataStyle' key={id}>     
                             <p>{firstname}</p>
