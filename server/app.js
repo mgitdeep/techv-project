03078f603ed7c160fbeb9b6eecf4bd957c370611
MKexplorer <mindblowingfacts0455@gmail.com>
2023-04-14 16:18:13 +0530

    Successfully established connection with Atlas

--- server/app.js
+++ server/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
+
+const DB = 'mongodb+srv://mkdb:MKdb%40123@cluster0.oth2bpd.mongodb.net/mernstack';
+
+mongoose.connect(DB).then(() => {
+    console.log('Connection with Remote Database is successful..!')         // mongoose.connect is a Promise hence we've used THEN and CATCH to handle the resolve & reject state respectively
+}).catch((err) => {
+    console.log(err)
+})
+
+const middleware = (req, res, next) => {
+    console.log('Middleware doing its job');
+    next();
+}
+
+// middleware()
 
 app.get("/", (req, res) => {
     res.send("Hello.. I'm a bot from server");
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", middleware, (req, res) => {
+    console.log('Inside the About section')
     res.send("Hello.. I'm a bot from About server");
-});
+}); 
 
 app.get("/contact", (req, res) => {
     res.send("Hello.. I'm a bot from Contact server");
