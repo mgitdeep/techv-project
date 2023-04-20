3b6f3fd072935b497c7eb5691cce6a31eba5abf1
MKexplorer <mindblowingfacts0455@gmail.com>
2023-04-20 12:04:06 +0530

    Successfully stored user data using Async-Await

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,46 +9,36 @@ router.get("/", (req, res) => {
     res.send("Hello.. I'm a bot from server - auth.js");
 })
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
 
     const {name, email, phone, work, password, cpassword} = req.body
 
-    // The below "if-else" is okay, but this is more efficient 
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "Please fill up the rest!"})
     }
 
-    Users.findOne({ email: email }).then((userExists) => {
-        if(userExists) {
+    try {
+        const userExists = await Users.findOne({ email: email })
+        if (userExists) {
             return res.status(422).json({ error: "Email already exist!"})
         }
-
-        // Else we're going to save the newly registred user data into Database - below you can also do > const user = new Users(req.body)
+        
         const user = new Users({name, email, phone, work, password, cpassword})
-        user.save().then(() => {
-            res.status(201).json({message: "User registered successfully!"})
-        }).catch((err) => {
-            res.status(500).json({error: "Failed to register!"})
-        })
- 
-    }).catch((err) => {
-        console.log(err)
-    })
 
-    // if (name && email && phone && work && password) {
-    //     console.log(name)
-    //     console.log(email)
-    // } else {
-    //     return res.status(422).json({ error: "Please fill up the rest!"})
-    // }
-    // console.log(req.body)
-    // console.log(req.body.name)
-    // console.log(req.body.email)
-    // console.log(name)
-    // console.log(email)
-    // console.log(work)
-    // res.send("Registration page is here")
-    // res.json({ message: req.body})              // If commented, will show "loading" in Postman
+        const userRegister = await user.save()
+        
+        res.status(201).json({message: "User registered successfully!"})
+        // if (userRegister) {
+        //     res.status(201).json({message: "User registered successfully!"})
+        // } else {
+        //     res.status(500).json({error: "Failed to register!"})
+        // }
+ 
+
+    } catch(err) {
+        console.log(err)
+    }
+    
 })
 
 module.exports = router
